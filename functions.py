69abfa511799bfe2583e49524b51e4a81b956b9f
Christian Alves <chralves@gmail.com>
2018-08-28 23:23:27 +0100

    Programing basics - Functions and Arguments

--- functions.py
+++ functions.py
@@ -16,6 +16,13 @@ def print_something2 (name = "Someone", age = "Unknown"): # Asignando las variab
     print("My name is " + name + " and my age is " + str(age))
     print("My name is", name, "and my age is", age)
 
+def print_people(*people): # Usamos un asterisco antes del argumentos para declarar un array (list). Argumentos infinitos
+    for person in people:
+        print("This person is", person)
+
+def do_math(num1, num2):
+    return num1 + num2
+
 my_function()
 
 second_function("Hello", "Christian")
@@ -25,4 +32,17 @@ print_something("Chistian", 34)
 
 print_something2() # Sin variables, imprime por defecto lo que hayamos atribuido... y no molesta diciendo que faltan variables
 
-print_something2("Christian") # Si sólo pasamos un argumento, coge el primero...
+print_something2("Christian") # Si sólo pasamos un argumento, coge el primero.
+
+print_something2(age = 34) # Para pasar el segundo argumento, usamos "Keywords"
+
+print_something2(age = 34, name = "Christian") # Incluso podemos pasar los argumentos en otro orden...
+
+print_people("Christian", "Joel", "Dámaris", "Lydia")
+
+do_math(5, 10) # Claro que si hacemos esto no hace nada...
+
+math1 = do_math(5, 7)
+math2 = do_math(11, 13)
+
+print("First sum is", math1, "and the second sum is", math2) # Si lo asignamos a una variable, podemos imprimir...
